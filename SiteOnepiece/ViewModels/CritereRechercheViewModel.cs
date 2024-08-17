9d72df0d4dec457c9cffcf53cdbc7483039b9d6a
gab <6221324@cegepmontpetit.ca>
2024-08-16 21:51:27 -0400

    transfere tp setting et viewstart/import

---
+++ SiteOnepiece/ViewModels/CritereRechercheViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace SiteOnepiece.ViewModels
+{
+    public class CritereRechercheViewModel
+    {
+        public string ChoixPourPersonnageVedette { get; set; }
+
+
+        public bool EstFruitLogia { get; set; }
+
+        public bool EstFruitZoan { get; set; }
+
+        public bool EstFruitParamecia { get; set; }
+
+        public int? MaxPrime { get; set; }
+
+        public int? MinPrime { get; set; }
+
+        public string? Nom { get; set; }
+
+        public CritereRechercheViewModel() 
+        {
+
+        }
+    }
+}
---
+++ SiteOnepiece/ViewModels/PageRechercheViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Primitives;
+using SiteOnepiece.ViewModels;
+
+namespace SiteOnepiece.ViewModels
+{
+    public class PageRechercheViewModel
+    {
+        public List<SiteOnepiece.Models.Enfant> Resultat { get; set; }
+        public CritereRechercheViewModel Criteres {  get; set; }
+
+        public PageRechercheViewModel() { }
+
+    }  
+}
